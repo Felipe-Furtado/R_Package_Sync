2848b791e172601c7b9a7b3ee8b0416a83805e2c
Furtado <FFURTADO1@mgh.harvard.edu>
2022-01-26 15:28:41 -0500

    Corrected loop to include all imported package names

--- Package_Sync.R
+++ Package_Sync.R
@@ -2,7 +2,7 @@
 packages <- read.csv("H:\\ff970_downloads\\installed_packages.csv")
 #Run
 packages_to_install <- NULL
-for (package_name in 1:length(packages)){
+for (package_name in 1:nrow(packages)){
    if (packages$Package[package_name] %in% installed.packages()==FALSE){
      packages_to_install <- c(packages_to_install, packages$Package[package_name])
    }
